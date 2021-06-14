ef04ba1e421392343cb958f1ae5ac9af07cbd677
Vitor Carlos <vitorcarlos1312@gmail.com>
2021-06-14 15:12:47 -0300

    finaly  15

--- src/components/produto/deletar/index.js
+++ src/components/produto/deletar/index.js
@@ -29,7 +29,7 @@ class DeletarProduto extends Component {
     componentDidMount() {
         const { id } = this.props.match.params;
 
-        fetch(`https://produtop1.herokuapp.com/produto/${id}`)
+        fetch(`${process.env.REACT_APP_API_URL}${id}`)
             .then((data) => {
                 data.json().then((data) => {
                     if (data.error) {
@@ -68,7 +68,7 @@ class DeletarProduto extends Component {
     handleClick = (event) => {
         const { id } = this.props.match.params;
 
-        fetch(`https://produtop1.herokuapp.com/produto/${id}`, {
+        fetch(`${process.env.REACT_APP_API_URL}${id}`, {
             method: "delete",
         })
             .then((data) => {
